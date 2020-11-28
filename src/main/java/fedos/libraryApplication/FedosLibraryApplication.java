a4e6eaa15c3e68d96acd7c59bcab708459a3a5df
Larin Fedor <larin@reksoft.ru>
2020-11-28 20:33:03 +0300

    add new project fedosLibrary

---
+++ src/main/java/fedos/libraryApplication/FedosLibraryApplication.java
@@ -0,0 +1,13 @@
+package fedos.libraryApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedosLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FedosLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fedos/libraryApplication/FedosLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package fedos.libraryApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedosLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
