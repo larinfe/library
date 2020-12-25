516071166989030cbf621eb39cfae367efff7707
Larin Fedor <larin@reksoft.ru>
2020-12-25 23:45:58 +0300

    add classes BookRepository.java and BookRepositoryImpl

---
+++ src/main/java/repository/BookRepository.java
@@ -0,0 +1,33 @@
+package repository;
+
+import entity.Book;
+
+
+import java.util.List;
+
+
+public class BookRepository {
+    public Book find(int id) {
+        return null;
+    }
+
+    public List<Book> findAll() {
+        return null;
+    }
+
+    public int update(Book b) {
+        return 0;
+    }
+
+    public int delete(int id) {
+        return 0;
+    }
+
+    public int insert(Book b) {
+        return 0;
+    }
+
+    public int count() {
+        return 0;
+    }
+}
---
+++ src/main/java/repository/BookRepositoryImpl.java
@@ -0,0 +1,20 @@
+package repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import query.BookQuery;
+
+@Repository
+
+public class BookRepositoryImpl extends BookRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public int count() {
+        return jdbcTemplate.queryForObject(BookQuery.COUNT, Integer.class);
+    }
+
+}
